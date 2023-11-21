80f70042472fb58cadc58aa5a4046c3f35072054
Jonathan Fambro <startek591@gmail.com>
2023-11-21 15:27:36 -0500

    Objects and bad behavior of objects

---
+++ 02_basics/myObjects.ts
@@ -0,0 +1,14 @@
+const User = {
+  name: "jonathan",
+  email: "fambrojonathan@gmail.com",
+  isActive: true,
+};
+
+function createUser({ name: string, isPaid: boolean }) {}
+
+let newUser = { name: "jonathan", isPaid: false, email: "j@h.com" };
+createUser(newUser);
+
+function createCourse(): { name: string; price: number } {
+  return { name: "reactjs", price: 399 };
+}
