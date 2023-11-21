38183e39a71361e445fcdf9f75e1981d1889bb22
Jonathan Fambro <startek591@gmail.com>
2023-11-21 15:16:24 -0500

    Better way to write function

--- 02_basics/myfunctions.ts
+++ 02_basics/myfunctions.ts
@@ -1,6 +1,5 @@
-function addTwo(num: number) {
-  // return num + 2;
-  return "hero";
+function addTwo(num: number): number {
+  return num + 2;
 }
 
 function getUpper(val: string) {
@@ -17,3 +16,28 @@ getUpper("jonathan");
 
 signUpUser("Jonathan", "fambro@gmail.com", false);
 loginUser("h", "h@gmail.com");
+
+function getValue(myVal: number) {
+  if (myVal > 5) {
+    return true;
+  }
+  return "200 OK";
+}
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
