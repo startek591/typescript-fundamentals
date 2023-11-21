9727587bd9de2b0b36ca780da8f0a128484319e6
Jonathan Fambro <startek591@gmail.com>
2023-11-21 14:31:19 -0500

    Number, boolean and type interence

--- 01_intro/variableMe.ts
+++ 01_intro/variableMe.ts
@@ -1,6 +1,12 @@
 let greetings: string = "Hello Jonathan";
 
-let mynum = 6;
-
 greetings.toLowerCase();
 console.log(greetings);
+
+// number
+
+let userId = 334466;
+
+// boolean
+
+let isLoggedIn: boolean = false;
