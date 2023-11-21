3227ca9ed94e796b6452d76f50ce11ac545f9dcd
Jonathan Fambro <startek591@gmail.com>
2023-11-21 14:56:57 -0500

    Knowing functions

---
+++ 02_basics/myfunctions.ts
@@ -0,0 +1,19 @@
+function addTwo(num: number) {
+  // return num + 2;
+  return "hero";
+}
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+addTwo(5);
+
+getUpper("jonathan");
+
+signUpUser("Jonathan", "fambro@gmail.com", false);
+loginUser("h", "h@gmail.com");
