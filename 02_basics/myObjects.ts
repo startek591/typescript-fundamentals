291444f381ec6d736c401d9455ccc2a28f820f79
Jonathan Fambro <startek591@gmail.com>
2023-11-21 15:37:28 -0500

    Type Aliases

--- 02_basics/myObjects.ts
+++ 02_basics/myObjects.ts
@@ -1,14 +1,26 @@
-const User = {
-  name: "jonathan",
-  email: "fambrojonathan@gmail.com",
-  isActive: true,
+// const User = {
+//   name: "jonathan",
+//   email: "fambrojonathan@gmail.com",
+//   isActive: true,
+// };
+
+// function createUser({ name: string, isPaid: boolean }) {}
+
+// let newUser = { name: "jonathan", isPaid: false, email: "j@h.com" };
+// createUser(newUser);
+
+// function createCourse(): { name: string; price: number } {
+//   return { name: "reactjs", price: 399 };
+// }
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
 };
 
-function createUser({ name: string, isPaid: boolean }) {}
-
-let newUser = { name: "jonathan", isPaid: false, email: "j@h.com" };
-createUser(newUser);
-
-function createCourse(): { name: string; price: number } {
-  return { name: "reactjs", price: 399 };
+function createUser(user: User) {
+  return { name: "", email: "", isActive: true };
 }
+
+createUser({ name: "", email: "", isActive: true });
