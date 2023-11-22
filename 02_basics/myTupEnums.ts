84a410efdc86ef29c4638a471b7dfc76239441d0
Jonathan Fambro <startek591@gmail.com>
2023-11-22 01:24:53 -0500

    Tuples

---
+++ 02_basics/myTupEnums.ts
@@ -0,0 +1,11 @@
+// const user: (string | number)[] = [1,"hc"];
+
+let tUser: [string, number, boolean];
+
+tUser = ["jf", 1223, true];
+
+let rgb: [number, number, number] = [255, 123, 112];
+
+type User = [number, string];
+
+const newUser: User = [112, "example@google.com"];
