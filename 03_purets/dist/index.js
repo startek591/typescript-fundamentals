d5d82bd149784b42e3929c1633b98ca15671fced
Jonathan Fambro <startek591@gmail.com>
2023-11-22 13:35:33 -0500

    Private Public

--- 03_purets/dist/index.js
+++ 03_purets/dist/index.js
@@ -1,9 +1,18 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Atlanta";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = "Atlanta";
         this.email = email;
         this.name = name;
+        this.city = "Atlanta";
     }
 }
 const jonathan = new User("jonathan@gmail.com", "Jonathan Fambro");
--- 03_purets/src/index.ts
+++ 03_purets/src/index.ts
@@ -1,11 +1,15 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "Atlanta";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
 class User {
-  email: string;
-  name: string;
   readonly city: string = "Atlanta";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  constructor(public email: string, public name: string) {}
 }
 
 const jonathan = new User("jonathan@gmail.com", "Jonathan Fambro");
