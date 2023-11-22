5f9ad577389da68626062cfc04ccf854138b8e33
Jonathan Fambro <startek591@gmail.com>
2023-11-22 00:51:56 -0500

    Array types

---
+++ 02_basics/myArray.ts
@@ -0,0 +1,16 @@
+const superHeros: string[] = [];
+const heroPower: Array<number> = [];
+
+superHeros.push("spiderman");
+heroPower.push(2);
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+allUsers.push({ name: "", isActive: true });
