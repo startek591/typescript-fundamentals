c62cd1465b3f3518534f62d43e2068813aaa1fca
Jonathan Fambro <startek591@gmail.com>
2023-11-22 13:47:36 -0500

    Getters and Setters

--- 03_purets/dist/index.js
+++ 03_purets/dist/index.js
@@ -12,7 +12,23 @@ class User {
     constructor(email, name) {
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
         this.city = "Atlanta";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get AppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const jonathan = new User("jonathan@gmail.com", "Jonathan Fambro");
--- 03_purets/src/index.ts
+++ 03_purets/src/index.ts
@@ -8,8 +8,28 @@
 //   }
 // }
 class User {
+  private _courseCount = 1;
   readonly city: string = "Atlanta";
   constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get AppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const jonathan = new User("jonathan@gmail.com", "Jonathan Fambro");
