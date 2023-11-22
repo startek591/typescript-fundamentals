9da986a3e4c19e732d06eb3aa93949801548ca0a
Jonathan Fambro <startek591@gmail.com>
2023-11-22 15:20:42 -0500

    Example of abstract classes

---
+++ 03_purets/src/abstractClass.ts
@@ -0,0 +1,24 @@
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+
+  abstract getSepia(): void;
+  getReelTime(): number {
+    return 8;
+  }
+}
+
+class Instragram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter);
+  }
+
+  getSepia(): void {
+    console.log("Sepia");
+  }
+}
+
+const jf = new Instragram("test", "Test", 3);
